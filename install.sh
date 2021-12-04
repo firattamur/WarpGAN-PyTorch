f912460ef82803d74d3abeb0542d0eb5d4e8dd4d
Fırat Tamur <ftamur16@ku.edu.tr>
2021-12-04 16:38:37 +0300

    chore: make gpu install default

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 # install pytorch 
-if [ "$1" = "gpu" ]; then
-    conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch -y
-else
+if [ "$1" = "cpu" ]; then
     conda install pytorch torchvision torchaudio cpuonly -c pytorch
+else
+    conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch -y
 fi
 
 # install numpy and matplotlib
