faac3e2dee0dbee79b78f9825d04dd89db85e4fa
Fırat Tamur <ftamur16@ku.edu.tr>
2022-01-10 04:28:09 +0300

    feat: add argparser and align dataset

--- train.py
+++ train.py
@@ -1,45 +1,20 @@
 """
-Training file for WarpGAN
+
+Train WarpGAN Model.
 
 """
 
-import argparse
 import sys
+from utils.commandline import load_config
+
+import torchvision.datasets as dset
 
 
-def main(args):
-    pass
-
-def parse_arguments(argv):
-
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("config_file", help="The path to the training configuration file", type=str)
-    
-    parser.add_argument("--name", help="Rename the log dir", type=str, default=None)
-    
-    parser.add_argument("--in_channels", help="Initial channels in the input images", type = int, default = 3)
-    
-    parser.add_argument("--n_classes", help = "Number of identities", type = int, default = None)
-    
-    parser.add_argument("--in_batch", help = "Number of samples in a batch", type = int, default = None)
-    
-    parser.add_argument("--in_height", help = "Height of the input images", type = int, default = 256)
-    
-    parser.add_argument("--in_width", help = "Width of the input images", type = int, default = 256)
-    
-    parser.add_argument("--style_size", help = "Sizes of style vector", type = int, default=8)
-    
-    parser.add_argument("--initial", help = "Value for convolution layer sizes", type = int, default = 64)
-    
-    parser.add_argument("--k", help = "k parameter in style controller", type = int, default = 64)
-    
-    parser.add_argument("--bottleneck_size", help = "Size of bottleneck in discriminator", type = int, default = 512)
-    
-    parser.add_argument("--n_ldmark", help = "", type = int, default = 16)
-    
-    return parser.parse_args(argv)
 
 if __name__ == "__main__":
 
-    main(parse_arguments(sys[1:]))
+    # load configuration file from specified configuration file path
+    config = load_config(sys.argv)
+    
+    # load dataset
+
