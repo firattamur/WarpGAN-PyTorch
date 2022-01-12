fix: fix gradient and optimizer errors for train file

losses/model_losses.py
@@ -40,9 +40,10 @@ class PatchAdversarialLoss(nn.Module):
         loss_D_A  = F.cross_entropy(input=logits_caric,  target=labels_D_A)
         loss_D_B  = F.cross_entropy(input=logits_photo,  target=labels_D_B)
         loss_D_BA = F.cross_entropy(input=logits_generated_caric, target=labels_D_BA)
+        
         loss_G    = F.cross_entropy(input=logits_generated_caric, target=labels_G_BA)
 
-        loss_D = loss_D_A + loss_D_B + loss_D_BA
+        loss_D    = loss_D_A + loss_D_B + loss_D_BA
 
         return loss_D, loss_G
 
@@ -96,8 +97,8 @@ class AdversarialLoss(nn.Module):
         loss_D_B  = F.cross_entropy(input=logits_photo,  target=labels_photo)
         loss_D_BA = F.cross_entropy(input=logits_generated_caric, target=labels_generated_caric)
 
-        loss_D = loss_D_A + loss_D_B + loss_D_BA
+        loss_D    = loss_D_A + loss_D_B + loss_D_BA
 
-        loss_G = F.cross_entropy(input=logits_generated_caric, target=labels_generated_caric)
+        loss_G    = F.cross_entropy(input=logits_generated_caric, target=labels_generated_caric)
 
         return loss_D, loss_G

models/m1layers_warpgan/conv2d.py
@@ -23,7 +23,7 @@ class CustomConv2d(nn.Module):
 
         self.conv_block = nn.Sequential(
             nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding="valid"),
-            activation(inplace=True)
+            activation()
         )
 
         self.initialize_weights()

models/m1layers_warpgan/deconv2d.py
@@ -23,7 +23,7 @@ class CustomDeConv2d(nn.Module):
 
         self.conv_block = nn.Sequential(
             nn.ConvTranspose2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride),
-            activation(inplace=True)
+            activation()
         )
 
         self.initialize_weights()

models/m2modules_warpgan/module_discriminator.py
@@ -123,7 +123,7 @@ class Discriminator(nn.Module):
 
         # inp: (in_batch, 3,  in_height/32,  in_width/32)
         # out: (in_batch * in_height/32 * in_width/32, 3)
-        patch5_logits = patch5_logits.reshape(-1, 3)
+        patch5_logits = patch5_logits.view(-1, 3)
 
         # Global Discriminator
         # inp: (in_batch, 512,  in_height/32,  in_width/32)

models/m2modules_warpgan/modules_decoder/controller_decoder.py
@@ -182,29 +182,29 @@ class DecoderController(nn.Module):
 
         # inp: (in_batch, initial*4, in_height, in_width)
         # out: (in_batch, initial*4, in_height, in_width)
-        out  = self.res1((x,   gamma, beta))
+        out1 = self.res1((x,   gamma, beta))
 
         # inp: (in_batch, initial*4, in_height, in_width)
         # out: (in_batch, initial*4, in_height, in_width)
-        out += self.res2(out)
+        out2 = out1[0] + self.res2(out1)[0]
         
         # inp: (in_batch, initial*4, in_height, in_width)
         # out: (in_batch, initial*4, in_height, in_width)
-        out += self.res3(out)
+        out3 = out2 + self.res3((out2, gamma, beta))[0]
 
         # inp: (in_batch, initial*4, in_height,   in_width)
         # out: (in_batch, initial,   in_height*4, in_width*4)
-        out  = self.deconvs(out[0])
+        out4 = self.deconvs(out3)
 
         # inp: (in_batch, initial,   in_height*4, in_width*4)
         # out: (in_batch, 3,         in_height*4, in_width*4)
-        out = self.conv(out)
+        out5 = self.conv(out4)
 
         # inp: (in_batch, 3,         in_height*4, in_width*4)
         # out: (in_batch, 3,         in_height*4, in_width*4)
-        images_rendered  = self.tanh(out)
+        images_rendered  = self.tanh(out5)
 
-        return out, images_rendered
+        return out5, images_rendered
 
 
     def initialize_weights(self) -> None:

models/m2modules_warpgan/modules_decoder/controller_style.py
@@ -100,7 +100,7 @@ class StyleController(nn.Module):
 
         # inp: (batch_size, 4 * k)
         # out: (batch_size, 4 * k, 1, 1)
-        gamma = torch.reshape(gamma, [-1, 4 * self.k, 1, 1])
+        gamma = gamma.view([-1, 4 * self.k, 1, 1])
         
         # inp: (batch_size, 128)
         # out: (batch_size, 4 * k, 1, 1)
@@ -108,7 +108,7 @@ class StyleController(nn.Module):
 
         # inp: (batch_size, 4 * k)
         # out: (batch_size, 4 * k, 1, 1)
-        beta = torch.reshape(beta, [-1, 4 * self.k, 1, 1])
+        beta = beta.view([-1, 4 * self.k, 1, 1])
         
         return beta, gamma
         

models/m2modules_warpgan/modules_decoder/controller_warp.py
@@ -95,7 +95,7 @@ class WarpController(nn.Module):
         landmarks_pred = self.linear2(out)
 
         # shape: (in_batch, n_ldmark * 2)
-        landmarks_pred += landmarks_mean
+        landmarks_pred = landmarks_pred + landmarks_mean
 
         # Displacements Prediction
 
@@ -105,7 +105,7 @@ class WarpController(nn.Module):
 
         # (in_batch, n_ldmark * 2) * (in_batch, 1)
         # out: (in_batch, n_ldmark * 2)
-        landmarks_displacement *= scales.view(-1, 1)
+        landmarks_displacement = landmarks_displacement * scales.view(-1, 1)
 
         # shape: (in_batch, n_ldmark, 2)
         landmarks_src = torch.reshape(landmarks_pred.detach().clone(), (-1, self.n_ldmark, 2))

models/m2modules_warpgan/modules_encoder/encoder_content.py
@@ -147,15 +147,15 @@ class ContentEncoder(nn.Module):
 
         # inp: (in_batch, initial * 4, in_height/4, in_width/4)
         # out: (in_batch, initial * 4, in_height/4, in_width/4)
-        out += self.res1(out)
+        out = out + self.res1(out)
 
         # inp: (in_batch, initial * 4, in_height/4, in_width/4)
         # out: (in_batch, initial * 4, in_height/4, in_width/4)
-        out += self.res2(out)
+        out = out + self.res2(out)
 
         # inp: (in_batch, initial * 4, in_height/4, in_width/4)
         # out: (in_batch, initial * 4, in_height/4, in_width/4)
-        out += self.res3(out)
+        out = out + self.res3(out)
 
         return out
         

models/model_warpgan.py
@@ -76,8 +76,8 @@ class WarpGANGenerator(nn.Module):
             images_photo = input["images_photo"]
             scales_photo = input["scales_photo"] 
 
-            encoded_photo, styles_B = self.encoder(images_photo)
-            generated_caric, _, _, _    = self.decoder(encoded_photo, scales_photo, None)
+            encoded_photo, styles_B  = self.encoder(images_photo)
+            generated_caric, _, _, _ = self.decoder(encoded_photo, scales_photo, None)
 
             return generated_caric
 