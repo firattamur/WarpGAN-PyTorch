e5b4a04cf1a95960851af49797c438becf73a33a
Fırat Tamur <ftamur16@ku.edu.tr>
2022-01-17 20:12:22 +0300

    feat: add device type to models

--- models/m2modules_warpgan/modules_decoder/controller_warp.py
+++ models/m2modules_warpgan/modules_decoder/controller_warp.py
@@ -33,6 +33,7 @@ class WarpController(nn.Module):
         self.in_height   = args.in_height
         self.in_width    = args.in_width
         self.in_channels = args.in_channels
+        self.device      = args.device
 
         # inp: (in_batch, in_channels, in_height, in_width)
         # out: (in_batch, in_channels, in_height, in_width)
@@ -88,7 +89,7 @@ class WarpController(nn.Module):
 
         # shape: (1, self.n_ldmark * 2)
         landmarks_mean = (torch.normal(mean=0, std=50, size=(self.n_ldmark, 2)) + \
-                          torch.tensor([0.5 * self.in_height, 0.5 * self.in_width])).flatten().type(dtype=torch.float32)
+                          torch.tensor([0.5 * self.in_height, 0.5 * self.in_width])).flatten().type(dtype=torch.float32).to(self.device)
         
         # inp: (in_batch, 128)
         # out: (in_batch, n_ldmark * 2)
@@ -108,8 +109,8 @@ class WarpController(nn.Module):
         landmarks_displacement = landmarks_displacement * scales.view(-1, 1)
 
         # shape: (in_batch, n_ldmark, 2)
-        landmarks_src = torch.reshape(landmarks_pred.detach().clone(), (-1, self.n_ldmark, 2))
-        landmarks_dst = torch.reshape((landmarks_pred + landmarks_displacement).detach().clone(), (-1, self.n_ldmark, 2))
+        landmarks_src = torch.reshape(landmarks_pred.detach().clone(), (-1, self.n_ldmark, 2)).to(self.device)
+        landmarks_dst = torch.reshape((landmarks_pred + landmarks_displacement).detach().clone(), (-1, self.n_ldmark, 2)).to(self.device)
 
         # shape: (1)
         landmarks_norm = torch.mean(torch.norm(landmarks_src - landmarks_dst, dim=(1, 2)))
